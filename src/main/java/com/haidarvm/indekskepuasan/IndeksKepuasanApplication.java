a04b7d340b808cb67822828796779599845911a9
haidarvm <haidarvm@gmail.com>
2019-07-19 22:39:00 +0700

    Initial Project add

---
+++ src/main/java/com/haidarvm/indekskepuasan/IndeksKepuasanApplication.java
@@ -0,0 +1,13 @@
+package com.haidarvm.indekskepuasan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndeksKepuasanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndeksKepuasanApplication.class, args);
+    }
+
+}
